80d427953d8fc25f2631041d41250be8c1939f6c
Zoltan Nagy <abesto@abesto.net>
2016-03-04 22:12:59 +0100

    Add more rules to default_rules

--- default_rules.py
+++ default_rules.py
@@ -6,13 +6,21 @@ def rules(build):
         build.FilesContain("~/.chef/**", "PRIVATE KEY", "Chef validator private key"),
         build.FilesExist("~/.chef/*encrypted*", "Potential Chef encrypted data bag key"),
         # AWS unified secrets
-        build.FilesExist("~/.aws/credentials", "Unified AWS credentials"),
-        # Old-style AWS secrets
+        build.FilesExist("~/.aws/credentials*", "Unified AWS credentials"),
+        # Old-style EC2 secrets
         build.FilesExist("~/.ec2/aws-credential", "AWS credentials"),
         build.FilesExist("~/.ec2/cert-amazon.pem", "AWS IAM certificate"),
         build.FilesExist("~/.ec2/pk-amazon.pem", "AWS IAM private key"),
+        # Old-style S3 secrets
+        build.FilesExist("~/.s3cmd", "AWS S3 configuration"),
         # Basic auth for PyPI servers
         build.FilesContain("~/.pip/pip.conf", "extra-index-url.*https?://.*:", "Basic auth password to a PyPI server"),
         # Gradle
-        build.FilesContain("~/.gradle/gradle.properties", "Password", "Package index credentials for Gradle")
+        build.FilesContain("~/.gradle/gradle.properties", "Password", "Package index credentials for Gradle"),
+        # Docker
+        build.FilesContain("~/.docker/config.json", "auth", "Docker registry credentials"),
+        # LastPass
+        build.FilesExist("~/.lpass/session_privatekey", "LastPass CLI session"),
+        # Vault
+        build.FilesExist("~/.vault_token", "HashiCorp Vault token")
     ]
